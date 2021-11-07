f6b79600e7c831fd40a77a8e47bd1acaabb911cd
Snooker4real <cindanojonathan@icloud.com>
2021-11-07 21:53:32 +0100

    adding db & fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,16 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        // Plus besoin de cette ligne à cause de l'injection de dépendance çi-dessus
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -26,9 +34,19 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/article/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show() {
-        return $this->render('blog/show.html.twig');
+    public function show(Article $article) {
+        // Même chose
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+
+        //$article = $repo->find($id);
+
+
+
+        return $this->render('blog/show.html.twig',
+        [
+            'article' => $article
+        ]);
     }
 }
